640358b97eaa75563ec38707bdbf7751fcbc4877
stavroskasidis <stavroskasidis@outlook.com>
2018-03-30 21:53:50 +0300

    Added gtk test project, updated nuspec

---
+++ Clipboard/Tests/Plugin.Clipboard.Tests.Gtk/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Gtk;
+using Plugin.Clipboard;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        var expected = "clipboard text";
+        CrossClipboard.Current.SetText(expected);
+        var actual = CrossClipboard.Current.GetTextAsync().Result;
+        if (actual != expected) throw new Exception($"Expected '{expected}', actual '{actual}'");
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
