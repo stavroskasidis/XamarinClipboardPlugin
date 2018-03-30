c97052bc83b6dd39fbe4597ae9854540c82eae21
stavroskasidis <stavroskasidis@outlook.com>
2018-03-30 23:53:38 +0300

    Added android UI test

---
+++ Clipboard/Tests/Plugin.Clipboard.UITests.Android/AppInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace Plugin.Clipboard.UITests.Android
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+#if DEBUG
+                string config = "Debug";
+#else
+                string config = "Release";
+#endif
+
+                return ConfigureApp
+                    .Android
+                    .ApkFile($@"..\..\..\..\TestProjects\Plugin.Clipboard.Tests.Android\bin\{config}\Plugin.Clipboard.Tests.Android.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
---
+++ Clipboard/Tests/Plugin.Clipboard.UITests.Android/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+using System.Threading.Tasks;
+
+namespace Plugin.Clipboard.UITests.Android
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp _app;
+        Platform _platform;
+        public Tests(Platform platform)
+        {
+            _platform = platform;
+        }
+
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _app = AppInitializer.StartApp(_platform);
+        }
+
+        [Test]
+        public async Task SetGetClipboard_RoundtripCase_ResultIsCorrect()
+        {
+            _app.Tap(x => x.Text("Run Tests"));
+            await Task.Delay(1000);
+            var query = _app.Query(x => x.Text("Passed"));
+
+            Assert.Greater(query.Length, 0, "Tests not passed");
+        }
+    }
+}
+
