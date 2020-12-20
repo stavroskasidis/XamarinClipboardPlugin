d54130187136e1457d793a420a860194fd6639f8
José Luis Marín Marín <joseluisxs@hotmail.com>
2020-12-20 13:47:38 +0100

    Added WPF backend

---
+++ Clipboard/Plugin.Clipboard.WPF/ClipboardImplementation.cs
@@ -0,0 +1,32 @@
+﻿using Plugin.Clipboard.Abstractions;
+using System.Threading.Tasks;
+
+namespace Plugin.Clipboard
+{
+    /// <summary>
+    /// Implementation for Clipboard
+    /// </summary>
+    public class ClipboardImplementation : IClipboard
+    {
+        public string GetText()
+        {
+            return GetTextInternal();
+        }
+
+        public Task<string> GetTextAsync()
+        {
+            return Task.FromResult(GetTextInternal());
+        }
+
+        public void SetText(string text)
+        {
+            System.Windows.Clipboard.SetText(text);
+        }
+
+        private string GetTextInternal()
+        {
+            if (System.Windows.Clipboard.ContainsText()) return System.Windows.Clipboard.GetText();
+            else return null;
+        }
+    }
+}
---
+++ Clipboard/Plugin.Clipboard.WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Plugin.Clipboard.WPF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Plugin.Clipboard.WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("f19a9079-8dfd-4ff7-8188-d1b009847077")]
