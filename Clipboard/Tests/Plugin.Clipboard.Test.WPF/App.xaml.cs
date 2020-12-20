10325a1d303ad9e1076fe1b589d577f16eea0bfc
José Luis <joseluisxs@hotmail.com>
2020-12-20 16:01:38 +0100

    Added WPF backend (#12)
    * Added WPF backend
    
    * Added WPF 4.7.2 projects
    
    * Update README.md
    
    * Updated version and nuspec

---
+++ Clipboard/Tests/Plugin.Clipboard.Test.WPF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Plugin.Clipboard.Test.WPF
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Clipboard/Tests/Plugin.Clipboard.Test.WPF/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace Plugin.Clipboard.Test.WPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var expected = "clipboard text";
+            CrossClipboard.Current.SetText(expected);
+            var actual = CrossClipboard.Current.GetTextAsync().Result;
+            if (actual != expected) throw new Exception($"Expected '{expected}', actual '{actual}'");
+        }
+    }
+}
